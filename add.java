c39fa23dd990533f59afe023cae75c181e353423
Soumyadip Paul <110677134+SOUMYADIP-FIEM@users.noreply.github.com>
2022-08-14 14:00:09 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add
+{
+     public static void main(String[] args)
+     {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter two numbers");
+         int m=sc.nextInt();
+         int n=sc.nextInt();
+         int p = m+n;
+         System.out.println("the rsult is :"+p);
+      }
+}
